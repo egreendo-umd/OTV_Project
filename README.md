051ae957f97ccc0fb4b578cd048837e804858a9b
Eamon Greendonahue <egreendo@terpmail.umd.edu>
2024-03-25 21:39:58 -0400

    Initialization

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # OTV_Project
+
+Create and develop on individual modules of the OTV, input and output is essential. 
+
+Main.ino is intended to be what the OTV actually runs, using the various individual programs as "libraries".
+
+Main: 
+
+Propulsion:
+
+Navigation:
+    - Currently messed up, base working code is still there but I have a lot to clean up.
+
+Overhead Vision System:
+
+Payload: 
+
+
+In case someone isn't familiar with the architecture format:
+
+Dividing by topic
+In general, divide your code along natural divisions in the program. Each file should contain a group of functions that do related things, e.g. manipulate the same data structure, are associated with the same class or closely-related group of classes, etc. Imagine another program that might use some of these functions: include in one file all the functions that this other program would require. Or try to write an English description of what's in each file: good divisions can usually be described succinctly.
+
+Examples:
+
+the set of functions that handle access to your database
+the set of functions that handle access a device (a serial port, the graphics screen)
+functions that do file or graphical input/output
+the implementation of an abstract data type, e.g. a linked list or a binary search tree
+a group of functions to do related numerical computations, e.g. a matrix manipulation package or a set of statistics functions
+A large program might be divided into several such files, plus a main program file. The main file contains the function main, as well as miscellaneous functions that don't seem to fit anywhere else and/or seem specialized to this particular program.
+
+There is no one "right" way to divide up a large program. Some programmers divide their programs into many, very tiny files. Some use a smaller number of larger files. The best choice depends not only on individual taste but on what sort of task the program is doing.
+
+- Harvey Mudd College
---
+++ src/navigation.ino
@@ -0,0 +1,202 @@
+//Include the motor driver library
+// #include <AFMotor.h>
+//Define the sensor pins
+#define S1Trig A0
+#define S2Trig A1
+#define S3Trig A2
+#define S1Echo A3
+#define S2Echo A4
+#define S3Echo A5
+//Set the speed of the motors
+#define Speed 160
+
+//Create objects for the motors
+// AF_DCMotor motorLeft(1);
+// AF_DCMotor motorRight(2);
+
+void setup() {
+  Serial.begin(9600);
+  //Set the Trig pins as output pins
+  pinMode(S1Trig, OUTPUT);
+  pinMode(S2Trig, OUTPUT);
+  pinMode(S3Trig, OUTPUT);
+  //Set the Echo pins as input pins
+  pinMode(S1Echo, INPUT);
+  pinMode(S2Echo, INPUT);
+  pinMode(S3Echo, INPUT);
+  //Set the speed of the motors
+  // motorLeft.setSpeed(Speed);
+  // motorRight.setSpeed(Speed);
+  int mode1 = True;
+  int mode2 = False;
+}
+
+void loop() {
+  int centerSensor = sensorTwo();
+  int leftSensor = sensorOne();
+  int rightSensor = sensorThree();
+// Check the distance using the IF condition
+
+// Query the Overhead Vision System
+  int mode = 1; // queryOVS(); // queryOVS will probably be an int array return?
+
+// Detect and Navigate to Pylon
+
+if (mode == 1) {
+
+  int width = 0;
+  int pylon_width = 12;
+  // Insert logic to detect pylon
+  // Determine a way to measure width of objects based on sensor positions
+  if (width > pylon_width*2) { // Since the pylon will be the only object other than the walls, we can be very coarse in our measurements
+    // Determine azimuth
+    // Navigate to center azimuth of measurement
+  }
+  // Insert logic to navigate to pylon, missile guidance may work?
+
+  if (8 >= centerSensor) {
+    Stop();
+    Serial.println("Stop");
+    // motorLeft.setSpeed(Speed/4);
+    // motorRight.setSpeed(Speed/4);
+    delay(500);
+    Serial.println("forward");
+    forward();
+    if (2 >= centerSensor) {
+      Stop();
+      Serial.println("Stop");
+
+      int success = deployPayload(); // Will need to create deployPayload() function
+
+      if (success == True) {
+        reverse();
+        Serial.println("Reverse");
+      
+        if (8 >= centerSensor) {
+          Stop();
+          Serial.println("Stop");
+          int azimuth = queryOVS(); // If it's an array, extract just the azimuth
+          int targetAzimuth = 90;
+          // Adjust to face towards the end goal zone
+          while (5 <= abs(azimuth - targetAzimuth)) {
+            left();
+            delay(200);
+            
+          }
+        }
+      }
+
+    }
+  }
+
+  continue;
+}
+
+// Obstacle Avoidance
+if (mode == 2) { 
+  if (8 >= centerSensor) {
+    Stop();
+    Serial.println("Stop");
+    delay(1000);
+    if (leftSensor > rightSensor) {
+      left();
+      Serial.println("Left");
+      delay(500);
+    } else {
+      right();
+      Serial.println("Right");
+      delay(500);
+    }
+  }
+}
+  Serial.println("Forward");
+  forward();
+  // Serial.print(leftSensor);
+  // Serial.print(", ");
+  // Serial.print(centerSensor);
+  // Serial.print(", ");
+  // Serial.print(rightSensor);
+  // Serial.print('\n');
+}
+
+// Query OVS for position
+int queryOVS() {
+  //int[] position = {150, 50};
+  int mode;
+
+  // Insert logic to query OVS through the ESP32 WiFi Modem
+
+  if (position[0] < 100) {
+    mode = 1;
+  } else {
+    mode = 2;
+  }
+  return mode;
+}
+
+//Get the sensor values
+int sensorOne() {
+  //pulse output
+  digitalWrite(S1Trig, LOW);
+  delayMicroseconds(4);
+  digitalWrite(S1Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(S1Trig, LOW);
+
+  long t = pulseIn(S1Echo, HIGH);//Get the pulse
+  int cm = t / 29 / 2; //Convert time to the distance
+  Serial.println(cm);
+  return cm; // Return the values from the sensor
+}
+
+//Get the sensor values
+int sensorTwo() {
+  //pulse output
+  digitalWrite(S2Trig, LOW);
+  delayMicroseconds(4);
+  digitalWrite(S2Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(S2Trig, LOW);
+
+  long t = pulseIn(S2Echo, HIGH);//Get the pulse
+  int cm = t / 29 / 2; //Convert time to the distance
+  Serial.println(cm);
+  return cm; // Return the values from the sensor
+}
+
+//Get the sensor values
+int sensorThree() {
+  //pulse output
+  digitalWrite(S3Trig, LOW);
+  delayMicroseconds(4);
+  digitalWrite(S3Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(S3Trig, LOW);
+
+  long t = pulseIn(S3Echo, HIGH);//Get the pulse
+  int cm = t / 29 / 2; //Convert time to the distance
+  Serial.println(cm);
+  return cm; // Return the values from the sensor
+}
+
+/*******************Motor functions**********************/
+void forward() {
+  // motorLeft.run(FORWARD);
+  // motorRight.run(FORWARD);
+}
+void left() {
+  // motorLeft.run(BACKWARD);
+  // motorRight.run(FORWARD);
+}
+void right() {
+  // motorLeft.run(FORWARD);
+  // motorRight.run(BACKWARD);
+}
+void Stop() {
+  // motorLeft.run(RELEASE);
+  // motorRight.run(RELEASE);
+}
+void reverse() {
+  // motorLeft.run(BACKWARD);
+  // motorRight.run(BACKWARD);
+}
