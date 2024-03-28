2789823a8bbb03d74793f30c920435fcba238d72
Eamon Greendonahue <egreendo@terpmail.umd.edu>
2024-03-28 02:30:08 -0400

    converted wifi module position query to OVS.cpp and OVS.h

---
+++ main/src/OVS/OVS.cpp
@@ -0,0 +1,26 @@
+#include "OVS.h"
+#include "Arduino.h"
+#include "params.h"
+#include "pinLayout.h"
+
+void initOVS() {
+    delay(2000);
+    // Assuming that the "9, 8" are pins, we'll need to edit the pinLayout
+    // then use those variable names for this function
+    OVS.begin("B-Team", DATA, 697, 9, 8);
+    delay(2000);
+    //rx=8 tx=9
+}
+void loop(){
+
+    // Where is this object getting created that's used below? Is the struct in the Enes100.begin cpp code?
+    OVS.updateLocation();
+    // xlocation=Enes100.getX();
+    //  ylocation=Enes100.getY();
+    OVS.print("x= " );
+    OVS.println(OVS.location.x);
+    OVS.print("y= " );
+    OVS.println(OVS.location.y);
+delay(100);
+
+}
---
+++ main/src/OVS/OVS.h
@@ -0,0 +1,12 @@
+// Copy the actual Enes100.h file info over please
+// If we need to use the Enes100.h file then we have to redo some things
+
+class OVS {
+    public {
+        void begin();
+        void updateLocation();
+    } private {
+
+    }
+}
+;
